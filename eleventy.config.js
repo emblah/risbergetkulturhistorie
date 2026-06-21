a0d5bb8fa91ec282d2c58ec3c6c6b53f987d3dd9
Marie Frogner <marieihlefrogner@gmail.com>
2026-06-21 19:48:41 +0200

    move generated page to docs

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,7 +4,7 @@ export default function (eleventyConfig) {
   return {
     dir: {
       input: "src",
-      output: "_site",
+      output: "docs",
       includes: "_includes",
       data: "_data"
     },
