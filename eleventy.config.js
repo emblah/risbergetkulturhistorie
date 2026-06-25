52da63b51af4f2e47590ebf30b1ffe76367cf977
Marie Frogner <marieihlefrogner@gmail.com>
2026-06-25 18:09:53 +0200

    remove path prefix and set domain to risbergetkulturhistorie.no

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,7 @@ import { HtmlBasePlugin } from "@11ty/eleventy";
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(HtmlBasePlugin);
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ "src/CNAME": "CNAME" });
 
   return {
     dir: {
